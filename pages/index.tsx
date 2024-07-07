c991ff57ac583aa00e0c0af04a169e448c4e1ede
神野凌太郎 <kaminoryoutarou@kaminoryoutarounoMacBook-Pro.local>
2024-07-07 20:39:53 +0900

    文言変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   return (
     <div className={styles.page}>
       <button onClick={handleClick} className={styles.button}>
-        ボタン
+        one more cat!
       </button>
       <div className={styles.frame}>
         {loading || <img src={imageUrl} className={styles.img} />}
